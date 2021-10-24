34a245153a263b0c73433032d845e6caaeac30c2
Cândea Cătălin <ecatalincandea@gmail.com>
2021-10-24 23:10:27 +0300

    Add EF to the project dependencies, add domain and data for the solution + a testing app to see if the connection and adding is working

---
+++ backend/VirtuWallyBackend/VirtuWally.Data/VirtuWallyContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using VirtuWally.Domain;
+
+namespace VirtuWally.Data
+{
+    public class VirtuWallyContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Doc> Docs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-NMANGOQ\\SQLEXPRESS;Initial Catalog=VirtuWally; Integrated Security=true;").LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information)
+            .EnableSensitiveDataLogging();
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Doc>()
+                .Property(d => d.UploadedDate)
+                .HasDefaultValueSql("getdate()");
+            modelBuilder.Entity<User>()
+               .Property(u => u.CreatedTime)
+               .HasDefaultValueSql("getdate()");
+        }
+    }
+}

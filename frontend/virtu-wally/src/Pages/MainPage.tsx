b1b8c969ca00d8cda75d5c46e9e6711a9676ef42
Cosmina <sas.cosmina99@gmail.com>
2021-11-04 22:30:22 +0200

    maybe working

--- frontend/virtu-wally/src/Pages/MainPage.tsx
+++ frontend/virtu-wally/src/Pages/MainPage.tsx
@@ -1,5 +1,9 @@
 import { FC } from "react";
 
+import Toolbar from "../components/Toolbar/Toolbar";
+import SideDrawer from "../components/SideDrawer/SideDrawer";
+import Backdrop from "../components/Backdrop/Backdrop";
+
 interface MainPageProps {
   visible: boolean;
   changePage: any;
@@ -10,6 +14,15 @@ export const MainPage: FC<MainPageProps> = ({ visible, changePage }) => {
     <>
       {visible && (
         <div>
+          <div style={{ height: "100%" }}>
+            <Toolbar />
+            <SideDrawer />
+            <Backdrop />
+            <main style={{ marginTop: "63px" }}>
+              <p> This is the main content</p>
+            </main>
+          </div>
+          <br /> <br />
           <button
             onClick={() => {
               changePage("Login");
---
+++ frontend/virtu-wally/src/components/Toolbar/Toolbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import DrawerToggleButton from "../SideDrawer/DrawerToggleButton";
+import "./Toolbar.css";
+
+const toolbar = (prop) => (
+  <header className="toolbar">
+    <nav className="toolbar-navigation">
+      <div>
+        <DrawerToggleButton />
+      </div>
+      <div className="toolbar-logo">
+        <a href="/">The Logo </a>
+      </div>
+      <div className="spacer"> </div>
+      <div className="toolbar-navigation-items">
+        <ul>
+          <li>
+            <a href="/">Products</a>
+          </li>
+          <li>
+            <a href="/">Users</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+);
+
+export default toolbar;

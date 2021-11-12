37ad1a8b36c1e31b7047b07263f61b8352b13c4b
Cosmina <sas.cosmina99@gmail.com>
2021-11-13 00:22:23 +0200

    elements for left and right drawer

--- frontend/virtu-wally/src/components/RightSideDrawer/RightDrawerToggleButton.tsx
+++ frontend/virtu-wally/src/components/RightSideDrawer/RightDrawerToggleButton.tsx
@@ -8,9 +8,10 @@ const RightDrawerToggleButton: FC<rightDrawerToggleButtonProps> = ({
   onClick,
 }) => (
   <button className="toggle-button" onClick={onClick}>
+    {/* <div className="toggle-button-line" />
     <div className="toggle-button-line" />
-    <div className="toggle-button-line" />
-    <div className="toggle-button-line" />
+    <div className="toggle-button-line" /> */}
+    <div className="align-items-center">User</div>
   </button>
 );
 
